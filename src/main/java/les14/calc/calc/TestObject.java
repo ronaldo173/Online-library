6d28734371b0db35653d397e1a293d363ba98490
Alexander <ronaldo173@inbox.ru>
2016-03-22 07:40:36 +0200

    temp

---
+++ src/main/java/les14/calc/calc/TestObject.java
@@ -0,0 +1,17 @@
+package les14.calc.calc;
+
+/**
+ * Created by Developer on 18.03.2016.
+ */
+public class TestObject {
+    private String name;
+
+    public TestObject(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
--- src/main/java/les14/calc/servlets/CalcServlet.java
+++ src/main/java/les14/calc/servlets/CalcServlet.java
@@ -2,6 +2,7 @@ package les14.calc.servlets;
 
 import les14.calc.calc.CalcOperations;
 import les14.calc.calc.OperationType;
+import les14.calc.calc.TestObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,7 +21,6 @@ import java.util.List;
 @WebServlet("/CalcServlet")
 public class CalcServlet extends HttpServlet {
 
-    private List<String> listOperations = new ArrayList<>();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -33,49 +33,79 @@ public class CalcServlet extends HttpServlet {
         doGet(req, resp);
     }
 
-    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-        resp.setContentType("text/html");
-        PrintWriter writer = resp.getWriter();
 
-        writer.println("<html>");
-        writer.println("<head>");
-        writer.println("<title>Servlet Calculator</title>");
-        writer.println("</head>");
-        writer.println("<body>");
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet CalcServlet</title>");
+        out.println("</head>");
+        out.println("<body>");
 
         try {
-            double one = Double.valueOf(req.getParameter("one").toString()).doubleValue();
-            double two = Double.valueOf(req.getParameter("two").toString()).doubleValue();
-            String operation = String.valueOf(req.getParameter("operation"));
 
-            HttpSession session = req.getSession(true);
-            OperationType operationType = OperationType.valueOf(operation.toUpperCase());
+            // считывание параметров
+            double one = Double.valueOf(request.getParameter("one"));
+            double two = Double.valueOf(request.getParameter("two"));
+            String opearation = request.getParameter("operation");
 
-            double result = calcRes(operationType, one, two);
+            // определение или создание сессии
+            HttpSession session = request.getSession(true);
 
+            request.getServletContext().setAttribute("obj", new TestObject("TestName"));
+
+
+            // получение типа операции
+            OperationType operType = OperationType.valueOf(opearation.toUpperCase());
+
+            // калькуляция
+            double result = calcRes(operType, one, two);
+
+            ArrayList<String> listOperations;
+
+            // для новой сессии создаем новый список
             if (session.isNew()) {
-                listOperations.clear();
+                listOperations = new ArrayList<String>();
+            }
+            else { // иначе получаем список из атрибутов сессии
+                listOperations = (ArrayList<String>) session.getAttribute("formula");
             }
 
-            listOperations.add(one + " " + operationType.getStringValue() + " " + two + " = " + result);
-            session.setAttribute("formula: ", listOperations);
+            // добавление новой операции в список и атрибут сессии
+            listOperations.add(one + " " + operType.getStringValue() + " " + two + " = " + result);
+            session.setAttribute("formula", listOperations);
 
-            writer.println("<h1>Id session: " + session.getId() + "</h1>");
-            writer.println("<h3>Operations amount: " + listOperations.size() + "</h3>");
 
-            for (String listOperation : listOperations) {
-                writer.print("<h4>" + operation + "</h4>");
+            // вывод всех операций
+            out.println("<h1>ID вашей сессии равен: " + session.getId() + "</h1>");
+
+            out.println("<h3>Список операций (всего:" + listOperations.size() + ") </h3>");
+
+            for (String oper : listOperations) {
+                out.println("<h3>" + oper + "</h3>");
             }
-        } catch (Exception e) {
-            System.out.println("error...");
+
+
+        } catch (Exception ex) {
+            // предупреждение пользователю в случае ошибки
+            out.println("</br>");
+            out.println(ex.toString());
+            out.println("</br>");
+            out.println("<h3 style=\"color:red;\">Возникла ошибка. Проверьте параметры</h3>");
+            out.println("<h3>Имена параметров должны быть one, two, operation</h3>");
+            out.println("<h3>Operation должен принимать 1 из 4 значений: add, subtract, multiply, divide</h3>");
+            out.println("<h3>Значения one и two должны быть числами</h3>");
+            out.println("<h1>Пример</h1>");
+            out.println("<h3>?one=1&two=3&operation=add</h3>");
+
         } finally {
-            writer.println("</body>");
-            writer.println("</html>");
-
-            if (writer != null) {
-                writer.close();
-            }
+            out.println("</body>");
+            out.println("</html>");
+            out.close();
         }
     }
 
